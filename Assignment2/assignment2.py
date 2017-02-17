769e6605d3cf2ec0c11b38c9350f19fdc8a4a6b1
Quang-Tam Huynh <quangtamh95@gmail.com>
2017-02-17 12:11:19 +0100

    Added comments to task b

--- Assignment2/assignment2.py
+++ Assignment2/assignment2.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+'''
 x0 = np.matrix('0.5; 0.5')
 e1x1 = np.matrix('0.9 0.0; 0.0 0.2')
 x1x0 = np.matrix('0.7 0.3; 0.3 0.7')
@@ -23,24 +23,27 @@ x2e12 = alpha * x2e12
 ##print "x1e1: ", x1e1
 #print "x2e1", x2e1
 #print "x2e12", x2e12
+'''
 
 def filtering(evidence):
-    uT = np.matrix('0.9 0.0; 0.0 0.2')
-    uF = np.matrix('0.1 0.0; 0.0 0.8')
-    xt1xt = np.matrix('0.7 0.3; 0.3 0.7')
-    if len(evidence) == 0:
-        return np.matrix('0.5; 0.5')
+    uT = np.matrix('0.9 0.0; 0.0 0.2')  # Chance of rain given umbrella
+    uF = np.matrix('0.1 0.0; 0.0 0.8')  # Chance of rain given no umbrella
+    xt1xt = np.matrix('0.7 0.3; 0.3 0.7')   # Chance that it rains given weather the day before
+    if len(evidence) == 0:  # Base case of the recursion
+        return np.matrix('0.5; 0.5')    # Chance of rain given no prior information is 50/50
     else:
-        x = xt1xt * filtering(evidence[0:len(evidence)-1])
-        if evidence[-1]:
+        x = xt1xt * filtering(evidence[0:len(evidence)-1])  # Recursive call
+        if evidence[-1]:    # If an umbrella is observed
             xe = uT * x
-        else:
+        else:               # If an umbrella is not observed
             xe = uF * x
-        xe = (1/np.sum(xe)) * xe
+        xe = (1/np.sum(xe)) * xe    # Normalization
         print xe
     return xe
 
+filtering([True, True, False, True, True])
 
-e = [True, True, False, True, True]
-filtering(e)
-#print e[0:len(e)-1]
+
+
+def forwardBackward(ev, prior):
+    sad = 0
