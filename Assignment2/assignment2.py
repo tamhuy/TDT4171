e9ab6d0c7f54bf18f4fe12a58944a7d6635091c0
Quang-Tam Huynh <quangtamh95@gmail.com>
2017-02-17 17:40:03 +0100

    completed task b and c, and added comments to the code

--- Assignment2/assignment2.py
+++ Assignment2/assignment2.py
@@ -1,49 +1,50 @@
 import numpy as np
-'''
-x0 = np.matrix('0.5; 0.5')
-e1x1 = np.matrix('0.9 0.0; 0.0 0.2')
-x1x0 = np.matrix('0.7 0.3; 0.3 0.7')
-#print(x0)
-#print(x1x0)
-x1 = x1x0 * x0
-x1e1 = e1x1 * x1
-alpha = 1/np.sum(x1e1)
-x1e1 = alpha*x1e1
 
-x2e1 = x1x0 * x1e1
+uT = np.array([[0.9, 0.0], [0.0, 0.2]]) # Chance of rain given umbrella
+uF = np.array([[0.1, 0.0], [0.0, 0.8]]) # Chance of rain given no umbrella
+xt1xt = np.array([[0.7, 0.3], [0.3, 0.7]])  # Chance that it rains given weather the day before
+prior = np.array([0.5, 0.5])
+ev = [True, True, False, True, True]    # Sequence of observations
+ev2 = [True, True]  # Used for checking if results are the same as in the book/slides
 
-x2e12 = e1x1 * x2e1
-#print x2e12
-alpha = 1/np.sum(x2e12)
-#print "alpha: ", alpha
-x2e12 = alpha * x2e12
-
-#print "alpha: ", alpha
-#print "x1: ", x1
-##print "x1e1: ", x1e1
-#print "x2e1", x2e1
-#print "x2e12", x2e12
-'''
-
-def filtering(evidence):
-    uT = np.matrix('0.9 0.0; 0.0 0.2')  # Chance of rain given umbrella
-    uF = np.matrix('0.1 0.0; 0.0 0.8')  # Chance of rain given no umbrella
-    xt1xt = np.matrix('0.7 0.3; 0.3 0.7')   # Chance that it rains given weather the day before
-    if len(evidence) == 0:  # Base case of the recursion
-        return np.matrix('0.5; 0.5')    # Chance of rain given no prior information is 50/50
+# Based on equation 15.5 on page 572 and on the lecture slides
+def forward(ev, prior):
+    if not ev:  # Base case of the recursion
+        return prior    # Chance of rain given no prior information is 50/50
     else:
-        x = xt1xt * filtering(evidence[0:len(evidence)-1])  # Recursive call
-        if evidence[-1]:    # If an umbrella is observed
-            xe = uT * x
-        else:               # If an umbrella is not observed
-            xe = uF * x
-        xe = (1/np.sum(xe)) * xe    # Normalization
-        print xe
-    return xe
+        x = np.dot(xt1xt, forward(ev[0:len(ev)-1], prior))  # Recursive call
+        if ev[-1]:      # If an umbrella is observed
+            xe = np.dot(uT, x)
+        else:           # If an umbrella is not observed
+            xe = np.dot(uF, x)
+        #print normalize(xe)
+    return normalize(xe)
 
-filtering([True, True, False, True, True])
+def normalize(x):
+    return x/x.sum()    # Normalization
 
+# Based on equation 15.9 on page 574 and on the lecture slides
+def backward(b, ev):
+    if ev:  # If an umbrella is observed
+        return np.dot(np.dot(uT, b), xt1xt)
+    else:   # If an umbrella is not observed
+        return np.dot(np.dot(uF, b), xt1xt)
 
-
+# Based on figure 15.4 on page 576
 def forwardBackward(ev, prior):
-    sad = 0
+    fv = np.array([None] * (len(ev) + 1))   # Initialize the vector of forward messages
+    sv = np.array([None] * (len(ev) + 1))   # Initialize the vector of backward messages
+    fv[0] = prior   # First known step
+    b = np.array([1.0, 1.0])    # A representation of backward messages
+    for i in range(1,len(ev) + 1):  # Using the forward function to fill the array with forward messages
+        fv[i] = forward([ev[i-1]], fv[i-1])
+    for i in range(len(ev), 0, -1): # Using the backward function to update backwards messages
+        print "\nIteration: ", i
+        print "b: ", b
+        sv[i] = normalize(np.multiply(fv[i], b))    # Normalize and update backwards messages
+        b = backward(b, [ev[i-1]])
+    print "sv: ", sv
+    return sv
+
+forward(ev, prior)          # Task B
+forwardBackward(ev, prior)  # Task C
