eb4cf7a34612d2506017b8dd36d911c9aa00b828
Quang-Tam Huynh <quangtamh95@gmail.com>
2017-02-18 15:26:21 +0100

    added viterbi

--- Assignment2/assignment2.py
+++ Assignment2/assignment2.py
@@ -7,6 +7,7 @@ prior = np.array([0.5, 0.5])
 ev = [True, True, False, True, True]    # Sequence of observations
 ev2 = [True, True]  # Used for checking if results are the same as in the book/slides
 
+
 # Based on equation 15.5 on page 572 and on the lecture slides
 def forward(ev, prior):
     if not ev:  # Base case of the recursion
@@ -17,11 +18,13 @@ def forward(ev, prior):
             xe = np.dot(uT, x)
         else:           # If an umbrella is not observed
             xe = np.dot(uF, x)
-        #print normalize(xe)
+        print normalize(xe)
     return normalize(xe)
+
 
 def normalize(x):
     return x/x.sum()    # Normalization
+
 
 # Based on equation 15.9 on page 574 and on the lecture slides
 def backward(b, ev):
@@ -30,21 +33,51 @@ def backward(b, ev):
     else:   # If an umbrella is not observed
         return np.dot(np.dot(uF, b), xt1xt)
 
+
 # Based on figure 15.4 on page 576
 def forwardBackward(ev, prior):
     fv = np.array([None] * (len(ev) + 1))   # Initialize the vector of forward messages
     sv = np.array([None] * (len(ev) + 1))   # Initialize the vector of backward messages
     fv[0] = prior   # First known step
     b = np.array([1.0, 1.0])    # A representation of backward messages
+    print "Forward messages"
     for i in range(1,len(ev) + 1):  # Using the forward function to fill the array with forward messages
         fv[i] = forward([ev[i-1]], fv[i-1])
-    for i in range(len(ev), 0, -1): # Using the backward function to update backwards messages
-        print "\nIteration: ", i
+    for i in range(len(ev), -1, -1): # Using the backward function to update backwards messages
+        print "Iteration: ", i
         print "b: ", b
         sv[i] = normalize(np.multiply(fv[i], b))    # Normalize and update backwards messages
         b = backward(b, [ev[i-1]])
-    print "sv: ", sv
+    print "Smoothed values: ", sv
     return sv
 
-forward(ev, prior)          # Task B
-forwardBackward(ev, prior)  # Task C
+
+def viterbi(ev, prior):
+    xT, xF = np.array([None] * len(ev)), np.array([None] * len(ev))
+    init = forward([ev[0]], prior)
+    xT[0], xF[0] = init[0], init[1]     # True
+
+    for i in range(1, len(ev)):
+        if ev[i]:
+            xT[i] = (uT[0][0] * max(xt1xt[0][0] * xT[i-1], xt1xt[0][1] * xF[i-1]))  # true
+            xF[i] = (uT[1][1] * max(xt1xt[0][1] * xT[i-1], xt1xt[0][0] * xF[i-1]))  # true
+        else:
+            xT[i] = (uF[0][0] * max(xt1xt[0][0] * xT[i-1], xt1xt[0][1] * xF[i-1]))  # false
+            xF[i] = (uF[1][1] * max(xt1xt[0][1] * xT[i-1], xt1xt[0][0] * xF[i-1]))  # false
+    print "xT: ", xT
+    print "xF: ", xF
+
+
+def main():
+    print "Forward: "
+    forward(ev, prior)         # Task B
+
+    print "\n", "-"*10
+    print "Forward-Backward: "
+    forwardBackward(ev, prior)  # Task C
+
+    print "\n", "-" * 10
+    print "Viterbi: "
+    viterbi(ev, prior)          # Task C
+
+main()
