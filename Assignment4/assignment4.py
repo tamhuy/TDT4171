ed87eb99f482b8ead8f5a15e30f3c975c609025b
Quang-Tam Huynh <quangtamh95@gmail.com>
2017-03-21 15:28:17 +0100

    argmax, b(q) and importance working

--- Assignment4/assignment4.py
+++ Assignment4/assignment4.py
@@ -6,8 +6,21 @@ def B(q):
         return 0
     return -(q * math.log(q, 2) + (1 - q) * math.log(1 - q, 2))
 
+
+def argmax(examples, attributes):
+    print "Argmax", "-" * 20
+    gain = []
+    for a in attributes:
+        #print a, "-" *10
+        #i = importance(a, examples)
+        gain.append(importance(a, examples))
+    #print asd.index(max(asd))
+    #print asd
+    return gain.index(max(gain))
+
+
 def importance(a, examples):
-    print B(25.0/75)
+    # print B(25.0/100)
     # E0 = [n0, p0]
     # E1 = [n1, p1]
     # E = [E0, E1] = [[n0, p0], [n1, p1]]
@@ -24,14 +37,16 @@ def importance(a, examples):
             E[1][1] += 1
     n = E[0][0] + E[1][0]
     p = E[0][1] + E[1][1]
-    print n, p
-    print E
-    print (p/(p+n))
+    #print "n, p: ",  n, p
+    print "E: ", E
+    # print B(p/(p+n))
     remainder = 0
     for j in range(2):
-        remainder += ((E[j][1] + E[j][0])/p + n) * B(E[j][1]/(E[j][1]+E[j][0]))
+        remainder += (E[j][1] + E[j][0])/(p + n) * B(E[j][1]/(E[j][1]+E[j][0]))
+        #print remainder
     gain = B(p/(p+n)) - remainder
-    print gain
+    #print B(p/(p+n))
+    return gain
 
 
 def plurality_value(a):
@@ -40,21 +55,20 @@ def plurality_value(a):
 def decision_tree_learning(examples, attributes, parent_examples):
     if not examples:
         return plurality_value(parent_examples)
-    elif examples:
-        return "Classification"
+    #elif examples:
+    #    return "Classification"
     elif not attributes:
         return plurality_value(examples)
     else:
-        A = 0
-        for a in attributes:
-            i = importance(a, examples)
-            if i > A:
-                A = i
+        A = argmax(examples, attributes)
+        print "A: ", A
         tree = A
+        """
         for vk in A:
             exs = something
             subtree = decision_tree_learning(exs, attributes-A, examples)
             #add branch to tree with label A= vk and subtree subtree
+        """
     return tree
 
 
@@ -75,7 +89,9 @@ for example in examples:
         count += 1
         print example
 print count
-attributes= [x for x in range(1, len(examples[0]))]
 print attributes
 '''
-importance(0, examples)
+attributes= [x for x in range(len(examples[0])-1)]
+print attributes
+#importance(0, examples)
+print decision_tree_learning(examples, attributes, 0)
